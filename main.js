b94bc57b18bdde989b8bab4c419c54d1a9714661
DarkkSorkk <120128914+DarkkSorkk@users.noreply.github.com>
2023-01-27 15:15:44 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,14 @@
 $(document).ready(function() {
-    // cache DOM elements
+  
     var $todoForm = $("#todo-form");
     var $taskInput = $("#task-input");
     var $taskList = $("#task-list");
-
-    // Event listener for form submission
     $todoForm.on("submit", function(event) {
         event.preventDefault();
         var taskName = $taskInput.val();
         $taskList.append("<li>" + taskName + "</li>");
         $taskInput.val("");
     });
-
-    // Event listener for task list items
     $taskList.on("click", "li", function() {
         $(this).toggleClass("completed");
     });
